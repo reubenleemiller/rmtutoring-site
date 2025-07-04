382789101e39d855142ef040ca4ba0d3e51cc6cd
reubenleemiller <159204043+reubenleemiller@users.noreply.github.com>
2025-07-04 17:35:23 -0600

    Update chatwoot.js

--- scripts/chatwoot.js
+++ scripts/chatwoot.js
@@ -1,5 +1,5 @@
-(function(d,t) {
-    var BASE_URL="https://app.chatwoot.com";
+  (function(d,t) {
+    var BASE_URL="https://chatwoot-production-58ea.up.railway.app";
     var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
     g.src=BASE_URL+"/packs/js/sdk.js";
     g.defer = true;
@@ -7,7 +7,7 @@
     s.parentNode.insertBefore(g,s);
     g.onload=function(){
       window.chatwootSDK.run({
-        websiteToken: 'fJF3PhKfiZ8DmtQm5HRLzCQ9',
+        websiteToken: 'HaHvx5RbQWm4ynZhwe7r8dwZ',
         baseUrl: BASE_URL
       })
     }
